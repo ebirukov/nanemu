7a137e2bc50dc29cad6dd2bb636845a855b5147d
ebirukov <info@mail.ru>
2025-09-18 12:04:46 +0300

    add version

--- cmd/nanemu/main.go
+++ cmd/nanemu/main.go
@@ -1,11 +1,23 @@
 package main
 
 import (
+	"debug/buildinfo"
+	"fmt"
 	"github.com/ebirukov/nanemu/internal/nanemu"
 	"log"
+	"os"
 )
 
 func main() {
+	if len(os.Args) > 1 && (os.Args[1] == "--version" || os.Args[1] == "-v") {
+		if info, err := buildinfo.ReadFile(os.Args[0]); err == nil {
+			fmt.Printf("Version: %s\n", info.Main.Version)
+			return
+		}
+
+		os.Exit(0)
+	}
+
 	app := nanemu.NewApp()
 
 	if err := app.Init(); err != nil {
