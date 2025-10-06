59322ebba432a45388f4ed3dd6f38d0cbe5f839a
ebirukov <info@mail.ru>
2025-10-06 06:40:42 +0300

    fix pass stdio

--- internal/nanemu/app.go
+++ internal/nanemu/app.go
@@ -111,7 +111,7 @@ func (app *App) Run() error {
 		Pdeathsig: syscall.SIGTERM,
 	}
 
-	app.qemuCmd.Stderr = os.Stderr
+	app.qemuCmd.Stderr, app.qemuCmd.Stdin = os.Stderr, os.Stderr
 
 	cmdStdOut, err := app.qemuCmd.StdoutPipe()
 	if err != nil {
